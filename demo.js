2c4de60dae817c56e4b3ff731f6503a3ba3d27df
ShweKhaPyaeZone-Winn <shwekha201@gmail.com>
2022-02-10 19:23:10 +0630

    'All'

---
+++ demo.js
@@ -0,0 +1,39 @@
+// console.log("Hello world");
+
+// var x = 60
+// if (x > 40) {
+// console.log("Success");}
+
+// var input = 7;
+
+// if(input%3===0 && input%5===0){
+//     console.log("FizzBuzz");
+// }else if(input%5===0){
+//     console.log("Buzz");
+// }else if(input%3===0){
+//     console.log("Fizz");
+// }else{
+//     console.log(input);
+// }
+
+// var grade = "A";
+
+// switch(grade){
+//     case "A":
+//         console.log("You get 'Destination' ");
+//         break;
+//     case "B":
+//         console.log("You get 'Pass'");
+//         break;
+//     default:
+//         console.log("You get 'Fail'");
+
+// }
+
+// function greetWithAlert(name){
+//     const a = name ;
+//     alert(a + ', Nay Kg Lar?');
+// }
+// greetWithAlert('SK');
+
+const projectName = 'tribute-page';
